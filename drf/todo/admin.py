0ca68b4e7ec0f0cd4b78396642f7ea66347f294f
geekwise-Jesus-Garcia <jesus1287garcia@yahoo.com>
2019-12-02 23:04:46 +0000

    started react

--- drf/todo/admin.py
+++ drf/todo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Todo
+from .models import Todo, Account
 
 # Register your models here.
 
@@ -8,3 +8,4 @@ class TodoAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Todo, TodoAdmin)
+admin.site.register(Account)
--- drf/todo/models.py
+++ drf/todo/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
     # Create your models here.
+
+class Account(models.Model):
+    product = models.CharField(max_length=100)
+    def _str_(self):
+        return(f"name {product.self}")
     
 class Todo(models.Model):
     title = models.CharField(max_length=120)
